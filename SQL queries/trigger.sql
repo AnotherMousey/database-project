fef848a0cdc63743e8de35764fcae81b80c436ae
mousey <hoangthanhbinh2006@gmail.com>
2024-12-14 17:21:36 +0700

    add some more triggers that are required

--- SQL queries/trigger.sql
+++ SQL queries/trigger.sql
@@ -12,4 +12,26 @@ BEGIN
     END IF;
 END$$
 
+CREATE TRIGGER beforeInsertIntoFlight 
+    BEFORE INSERT ON flight
+    FOR EACH ROW
+BEGIN
+    -- Validate that timeFrom is before timeTo
+    IF TIMESTAMP(NEW.dateFrom, NEW.timeFrom) > TIMESTAMP(NEW.dateTo, NEW.timeTo) THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'ERROR: timeFrom must not be after timeTo';
+    END IF;
+END$$
+
+CREATE TRIGGER beforeInsertIntoReservation
+    BEFORE INSERT ON reservation
+    FOR EACH ROW
+BEGIN
+    -- Validate that reservationTime is before timeTo
+    IF (SELECT TIMESTAMP(f.dateFrom, f.timeFrom) FROM flight f WHERE f.flightID = NEW.flightID) < NEW.reservationTime THEN
+		SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'ERROR: reservationTime must be before the time the flight fly';
+    END IF;
+END$$
+
 DELIMITER ;
